8576a9fe12d9a22cb49581617b783b1b176d097d
Zhixin Ling <1069066484@qq.com>
2019-05-06 16:58:27 +0800

    Update LExample.py

--- LExample.py
+++ LExample.py
@@ -5,5 +5,7 @@
 """
 1.创建的每一个文件都以自己的姓的首字母开头，比如Lxxx.py, Hxxx.py, Dxxx.py. Lxxx.npy, Hxxx.npy, Dxxx.png. 防止重名的情况出现。
 2.在本地修改代码，将正确的代码上传至repository中。
-3.一般我们只是上传代码。Github不支持超过100M的文件。对于需要共享的小文件（图片，npy）等等，不要放在主目录（master branch）中。创建一个文件夹，如Lsaving去储存。
-4.下载一个Github for Windows，每当自己可以方便地完成本地代码与GitHub仓库代码的同步。我们现在都在master branch下工作，确保自己上传的代码的版本都是可用的。
+3.一般我们只是上传代码。Github不支持超过100M的文件。对于需要共享的小文件（图片，npy）等等，不要放在主目录（master branch）中。
+  创建一个文件夹，如Lsaving去储存。
+4.下载一个Github for Windows，每当自己可以方便地完成本地代码与GitHub仓库代码的同步，pull origin同步至本地，commit to master
+  同步至GitHub仓库。我们现在都在master branch下工作，确保自己上传的代码的版本都是可用的。
