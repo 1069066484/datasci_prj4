4a6309605379e89b5b6279ab40a68ff31def5733
Zhixin Ling <1069066484@qq.com>
2019-05-10 17:18:10 +0800

    Update Ldata_helper.py

--- Ldata_helper.py
+++ Ldata_helper.py
@@ -15,7 +15,7 @@ import csv
 
 def read_mnist(one_hot=True):
     from tensorflow.examples.tutorials.mnist import input_data
-    return input_data.read_data_sets(global_defs.PATH_MNIST,one_hot=one_hot)
+    return input_data.read_data_sets(os.path.join(global_defs.PATH_MNIST),one_hot=one_hot)
 
 
 def posfix_filename(filename, postfix):
@@ -49,7 +49,7 @@ def read_labeled_features(csvfn):
     arr = csvfile2nparr(csvfn)
     data, labels = np.hsplit(arr,[-1])
     labels = labels.reshape(labels.size)
-    return data, labels
+    return [data, labels]
 
 
 def read_paired_labeled_features(type_DA):
@@ -57,12 +57,12 @@ def read_paired_labeled_features(type_DA):
     path_src_dst_da_lf = pkfilename(src_csv + "__" + dst_csv)
     path_src_dst_da_lf = os.path.join(global_defs.PATH_RAW_DL_FEATURES, path_src_dst_da_lf)
     if os.path.exists(path_src_dst_da_lf):
-        return pickle.load(open(path_src_dst_da_lf, 'rb'))
+        return list(pickle.load(open(path_src_dst_da_lf, 'rb')))
     src_csv = csvfilename(os.path.join(global_defs.PATH_RESNET50_FOLDER, src_csv))
     dst_csv = csvfilename(os.path.join(global_defs.PATH_RESNET50_FOLDER, dst_csv))
     src_dst_da_lf = [read_labeled_features(src_csv), read_labeled_features(dst_csv)]
     pickle.dump(src_dst_da_lf, open(path_src_dst_da_lf, 'wb'))
-    return src_dst_da_lf
+    return list(src_dst_da_lf)
 
 
 def plt_show_it_data(it_data, xlabel='iterations', ylabel=None, title=None, do_plt_last=True):
@@ -99,9 +99,28 @@ def index_split(num, percent1):
     nums = np.random.permutation(num)
     return [nums[:percent1], nums[percent1:]]
 
+
+def labeled_data_split(labeled_data, percent_train=0.6):
+    train_idx, test_idx = index_split(labeled_data[0].shape[0], percent_train)
+    train_ld = [labeled_data[0][train_idx], labeled_data[1][train_idx]]
+    test_ld = [labeled_data[0][test_idx], labeled_data[1][test_idx]]
+    return [train_ld, test_ld]
+
+
 def rand_arr_selection(arr, num):
     nonrep_rand_nums = non_repeated_random_nums(arr.shape[0], num)
     return [arr[nonrep_rand_nums], nonrep_rand_nums]
+
+
+def labels2one_hot(labels):
+    labels = np.array(labels, dtype=np.int)
+    if len(labels.shape) == 1:
+        minl = np.min(labels)
+        labels -= minl
+        maxl = np.max(labels) + 1
+        r = range(maxl)
+        return np.array([[1 if i==j else 0 for i in r] for j in labels])
+    return labels
 
 
 def _get_dicts_test():
@@ -109,9 +128,18 @@ def _get_dicts_test():
     print(id2name, name2id)
 
 
-if __name__ == '__main__':
+def _test_read_paired_labeled_features():
     [src_dl, dst_dl] = read_paired_labeled_features(global_defs.DA.A2R)
     print(src_dl[0].shape)
     print(src_dl[1].shape)
     print(dst_dl[0].shape)
     print(dst_dl[1].shape)
+
+
+def _test_labels_one_hot():
+    a = np.array([2,1,0,0,0,2,1,1,1])
+    print(labels2one_hot(a))
+
+
+if __name__ == '__main__':
+    _test_labels_one_hot()
